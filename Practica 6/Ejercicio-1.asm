955492e6acbdacf8ede335fdd3ae06b1e335c333
CarlosCastanon2099 <108638686+CarlosCastanon2099@users.noreply.github.com>
2023-04-22 23:09:38 -0600

    Update Ejercicio-1.asm

--- Practica 6/Ejercicio-1.asm
+++ Practica 6/Ejercicio-1.asm
@@ -1,15 +1,17 @@
+# 1) Escribe un programa que, sin usar la memoria ni la instruccion move, copie el contenido de un registro a otro.
 .text
 .globl main
 
 main:
   # 1) 
-  # xor $v0, $t0, $zero
+  # Como no encontramos otra manera de mover registros por consola, nuestra solucion fue la de que en 
+  # los registros en los que no se deba usar move para moverlos, basta con usar las operaciones add y sub 
+  # para obtener un comportamiento bastnate parecido a move.
+  
+  # Ejemplo: 
   add $v0, $t1, $zero   # copia el contenido de $t1 en $v0
-  sub $v0, $v0, $zero   # limpia el registro $v0
+  sub $t1, $t1, $zero   # limpia el registro $t1 para que los datos solo existan en $v0
   
-  
-  
-
   
   # termina el programa
   li $v0, 10
